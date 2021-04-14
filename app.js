26d85fd0abd2e5249b4c7503ef413b9823ca3639
Suhaibmasalha <>
2021-04-14 11:10:15 +0000

    add app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+alert('Hello from alert');
+var Age = prompt ('How old are you?');
+
+if ( Age >= 18 )
+{
+  alert ('Welcome to my page');
+}
+
+else 
+{
+alert ("Sorry, but you can't reach this page")
+}
+
+
+
+
+document.write('<img src="" >' );
+
+
+var urTime = prompt ('What time is it now');
+
+if ( urTime >= 18 )
+{
+  alert ('Good Evening');
+  document.write('<img src="https://www.wishesmsg.com/wp-content/uploads/Good-Evening-Message-1280x720.jpg" >' );
+
+}
+
+else if ( urTime >= 12 )
+{
+  alert ('Good Afternoon');
+  document.write('<img src="https://thumbs.dreamstime.com/b/close-up-message-good-afternoon-handwritten-whiteboard-good-afternoon-168764032.jpg" >' );
+}
+
+else if ( urTime >= 0 )
+{
+  alert ('Good Morning');
+  document.write('<img src="https://i.ytimg.com/vi/JsuqnzqZimQ/maxresdefault.jpg" >' );
+}
---
+++ temp.js
@@ -0,0 +1,76 @@
+
+//I'm consoling my name
+console.log('Hello its JS');
+console.log('Hello from app.js');
+
+// single line comment
+
+/*
+Mul lines of comments 
+*/
+
+
+
+
+/*
+
+Var in js
+var theName = 'Sohaib';
+
+*/
+
+
+
+/*
+Datatybes
+1. String --> text inside ' ' or " "
+what I write I see
+
+2. Number 
+- int
+- float ... etc.
+
+2. Number 
+- int
+- float ... etc.
+
+3. Boolean 
+- true 
+- false 
+
+4. arithmatic operators
+
++ add
+- sub 
+* mul 
+/ devide
+% mod 
+** power 
+
+*/
+
+
+//String
+console.log('Hello from app.js');
+//numbers
+console.log(1);
+console.log(1+3+8);
+//Boolean 
+console.log(true);
+console.log(false);
+
+// add
+console.log('adding', 1+3+8);
+
+//sub 
+console.log('sub', 1+3+8);
+
+//mul 
+console.log('sub', 1*3*8);
+
+//power to 
+console.log('2^2 =',2 ** 2);
+
+// concatianation I will use it to add 2 strings together using 
+console.log('hello'+' '+'Sohaib');
+
